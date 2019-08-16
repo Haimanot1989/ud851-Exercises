9ebc4954406d8428cc8230f2b48510c98fc5a12d
Haimanot1989 <haimif@yahoo.com>
2019-08-16 14:23:15 +0200

    Lesson 3 - RecyclerView - RecyclerViewClickHandling

---
+++ Lesson03-Green-Recycler-View/T03.07-Exercise-RecyclerViewClickHandling/app/src/main/java/com/example/android/recyclerview/ListItemClickListener.java
@@ -0,0 +1,7 @@
+package com.example.android.recyclerview;
+// DONE (1) Add an interface called ListItemClickListener
+// DONE (2) Within that interface, define a void method called onListItemClick that takes an int as a parameter
+
+public interface ListItemClickListener {
+    void onListItemClick(int param);
+}
